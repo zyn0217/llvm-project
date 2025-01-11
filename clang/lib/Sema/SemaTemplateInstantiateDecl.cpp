c93f499fe6e270c4c079978ad60c100c2bb7380f
Younan Zhang <zyn7109@gmail.com>
2025-01-11 22:27:45 +0800

    negative, sorry

--- clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
+++ clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
@@ -5317,7 +5317,7 @@ void Sema::InstantiateFunctionDefinition(SourceLocation PointOfInstantiation,
       return Context.isUnevaluated() || Context.isImmediateFunctionContext();
     });
   }();
-  if (ShouldSkipCG) {
+  if (!ShouldSkipCG) {
     DeclGroupRef DG(Function);
     Consumer.HandleTopLevelDecl(DG);
   }
